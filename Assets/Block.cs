6719f603757b9d21fc42c7f4737d753e344ef449
andrei-trifonov <rsanddrs-company@yandex.ru>
2021-09-27 22:05:37 +0300

    1.0
    Start

---
+++ Assets/Block.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public bool placed;
+    public bool protect;
+    public bool actual_spawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (actual_spawn){
+        gameObject.GetComponent<BoxCollider>().enabled = false;
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+}
---
+++ Assets/SwipeController.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeController : MonoBehaviour
+{
+    public  bool tap, swipeLeft, swipeRight, swipeUp, swipeDown;
+    public bool isDraging = false;
+    public Vector2 startTouch, swipeDelta;
+    public GameObject Rotor;
+    public float swipeDeltaoldz;
+    public float swipeDeltaoldy;
+    public float grade;
+    public bool active;
+    public float speed;
+    public GameObject trans;
+    void Start(){
+    tap = swipeDown = swipeUp = swipeLeft = swipeRight = false;}
+
+    IEnumerator TestCoroutine()
+    {
+
+    		yield return new WaitForSeconds(1.5f);
+    		active = false;
+    		 Rotor.transform.rotation = trans.transform.rotation;
+    		Debug.Log(Time.deltaTime);
+
+    }
+    private void Update()
+    {
+        if (active){
+           Rotor.transform.rotation = Quaternion.Slerp(Rotor.transform.rotation, trans.transform.rotation ,  Time.deltaTime * speed);
+
+
+        }
+
+
+        #region ПК-версия
+        if (Input.GetMouseButtonDown(0))
+        {
+ if (!active){
+
+            tap = true;
+            isDraging = true;
+
+            startTouch = Input.mousePosition;
+        }}
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isDraging = false;
+            Reset();
+        }
+        #endregion
+        #region Мобильная версия
+        if (Input.touches.Length > 0)
+        {
+            if (Input.touches[0].phase == TouchPhase.Began)
+            {
+                if (!active){
+                tap = true;
+                isDraging = true;
+
+                startTouch = Input.touches[0].position;
+            }
+            else if (Input.touches[0].phase == TouchPhase.Ended || Input.touches[0].phase == TouchPhase.Canceled)
+            {
+                isDraging = false;
+
+            }
+        }
+        #endregion
+
+        //Просчитать дистанцию
+        swipeDelta = Vector2.zero;
+        if (isDraging)
+        {
+            if (Input.touches.Length < 0)
+                swipeDelta = Input.touches[0].position - startTouch;
+            else if (Input.GetMouseButton(0))
+                swipeDelta = (Vector2)Input.mousePosition - startTouch;
+        }
+
+        //Проверка на пройденность расстояния
+        if (swipeDelta.magnitude > 40)
+        {
+
+            //Определение направления
+            float x = swipeDelta.x;
+            float y = swipeDelta.y;
+            if (Mathf.Abs(x) > Mathf.Abs(y))
+            {
+
+                if (x < 0){
+                    swipeLeft = true;
+
+
+
+                                     }
+
+                else {
+
+
+                    swipeRight = true;
+            }}
+            else
+            {
+
+                if (y < 0){
+                    swipeDown = true;
+
+                                                                          }
+                else{
+
+
+                    swipeUp = true;}
+            }
+
+
+        }
+
+    }
+
+    private void Reset()
+    {
+
+        startTouch = swipeDelta = Vector2.zero;
+        isDraging = false;
+
+
+                        if (swipeDown){
+                        swipeDown = false;
+                        trans.transform.rotation = Rotor.transform.rotation;
+                         trans.transform.Rotate((int)-90,(int)0, (int)0,Space.World);
+                         active = true;
+
+                         }
+
+
+
+
+                       if (swipeLeft){
+                       swipeLeft = false;
+                       trans.transform.rotation = Rotor.transform.rotation;
+                        trans.transform.Rotate((int)0, (int)90, (int)0,Space.World);
+                        active = true;
+                         }
+                        if (swipeRight){
+                        swipeRight = false;
+                         trans.transform.rotation = Rotor.transform.rotation;
+                         trans.transform.Rotate((int)0, (int)-90, (int)0,Space.World);
+                         active = true;}
+                        if (swipeUp){
+                         trans.transform.rotation = Rotor.transform.rotation;
+                                                 trans.transform.Rotate((int)90,(int)0, (int)0,Space.World);
+                                                 active = true;
+                        swipeUp = false;
+
+                         }
+StartCoroutine(TestCoroutine());
+                }
+
+}
